e4f369ef6e039f550b750632d7e0d1cc32409dc7
Andrew Baldwin <andrew.baldwin44@gmail.com>
2020-08-14 19:31:06 -0400

    Add firebase authentication context

---
+++ client/src/components/AuthenticationContext.js
@@ -0,0 +1,79 @@
+import React, { createContext, useState } from 'react';
+
+import withFirebaseAuth from 'react-with-firebase-auth';
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
+
+export const AuthenticationContext = createContext(null);
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAJsxWPJggGuH1ZsRt5wT1gO3onS4zF-P0",
+  authDomain: "final-project-96d37.firebaseapp.com",
+  databaseURL: "https://final-project-96d37.firebaseio.com",
+  projectId: "final-project-96d37",
+  storageBucket: "final-project-96d37.appspot.com",
+  messagingSenderId: "268524372667",
+  appId: "1:268524372667:web:14fdadaa80c76a09f7df9e"
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const firebaseAppAuth = firebaseApp.auth();
+const googleProvider = new firebase.auth.GoogleAuthProvider();
+
+function createUserWithEmail(email, password) {
+  return firebase.auth().createUserWithEmailAndPassword(email, password);
+}
+
+function signInWithEmail(email, password) {
+  return firebase.auth().signInWithEmailAndPassword(email, password);
+}
+
+function signInWithGoogle() {
+  return firebase.auth().signInWithPopup(googleProvider);
+}
+
+const AuthenticationProvider = ({ children, signOut, user }) => {
+  const [appUser, setAppUser] = useState({});
+  const [idToken, setIdToken] = useState(null);
+  const [message, setMessage] = useState('');
+
+  const handleSignOut = () => {
+    signOut();
+    setAppUser({});
+  }
+
+  const retrieveClientID = () => {
+    if (firebase.auth().currentUser) {
+      firebase.auth().currentUser.getIdToken(true)
+        .then(idToken => setIdToken(idToken))
+        .catch(error => console.log('unauthorized'));
+    }
+    else {
+      setIdToken('unauthorized');
+    }
+  }
+
+  firebase.auth().onAuthStateChanged(() => {
+    retrieveClientID();
+  });
+
+  return (
+    <AuthenticationContext.Provider
+      value={{
+        appUser,
+        createUserWithEmail,
+        signInWithEmail,
+        signInWithGoogle,
+        handleSignOut,
+        idToken,
+        message,
+      }}
+    >
+      {children}
+    </AuthenticationContext.Provider>
+  );
+};
+
+export default withFirebaseAuth({
+  firebaseAppAuth,
+})(AuthenticationProvider);
--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from "react-redux";
 
+import AuthenticationProvider from './components/AuthenticationContext';
+
 import App from './components/App';
 
 import configureStore from "./store";
@@ -9,8 +11,10 @@ import configureStore from "./store";
 const store = configureStore();
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <AuthenticationProvider>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </AuthenticationProvider>,
   document.getElementById('root')
 );
