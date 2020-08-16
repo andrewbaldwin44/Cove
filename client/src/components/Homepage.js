1a52ef722f72c5e4ae149785885a744e730f032e
Andrew Baldwin <andrew.baldwin44@gmail.com>
2020-08-16 10:44:18 -0400

    Create new room dialog component

--- client/src/components/Homepage.js
+++ client/src/components/Homepage.js
@@ -1,38 +1,12 @@
-import React, { useState, createRef } from 'react';
-import { useHistory } from 'react-router-dom'
+import React, { useState } from 'react';
 import styled from 'styled-components';
 
-import Dialog from '@material-ui/core/Dialog';
-import DialogActions from '@material-ui/core/DialogActions';
-import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
-import DialogTitle from '@material-ui/core/DialogTitle';
-import Paper from '@material-ui/core/Paper';
-import Draggable from 'react-draggable';
-
-import { ReactComponent as DoorIcon } from '../assets/images/door.svg';
-
-function DraggableDialog(props) {
-  return (
-    <Draggable>
-        <Paper {...props} />
-    </Draggable>
-  )
-}
+import NewRoomDialog from './NewRoomDialog';
 
 function Homepage() {
-  const [open, setOpen] = useState(false);
-  const history = useHistory();
-  const roomNameInput = createRef();
+  const [openDialog, setOpenDialog] = useState(false);
 
-  const configureNewRoom = () => setOpen(true);
-  const handleClose = () => setOpen(false);
-
-  const createNewRoom = () => {
-    const roomName = roomNameInput.current.value;
-    
-    history.push(`rooms/room/${roomName}`);
-  }
+  const configureNewRoom = () => setOpenDialog(true);
 
   return (
     <>
@@ -41,33 +15,10 @@ function Homepage() {
       >
         Add +
       </Add>
-      <StyledDialog
-        open={open}
-        onClose={handleClose}
-        PaperComponent={DraggableDialog}
-        aria-labelledby="draggable-dialog-title"
-      >
-        <DialogTitle style={{ cursor: 'move' }} id="draggable-dialog-title">
-          New Room
-        </DialogTitle>
-        <DialogContent>
-          <DialogContentText>
-            Choose a Room Name to get Started!
-          </DialogContentText>
-          <StyledInput
-            type='text'
-            ref={roomNameInput}
-          />
-        <DialogActions>
-          <SubmitButton
-            onClick={createNewRoom}
-          >
-            <StyledDoorIcon />
-            Open
-          </SubmitButton>
-        </DialogActions>
-        </DialogContent>
-      </StyledDialog>
+      <NewRoomDialog
+        openDialog={openDialog}
+        setOpenDialog={setOpenDialog}
+      />
     </>
   )
 }
@@ -84,36 +35,6 @@ const Add = styled.button`
   border: 5px dashed var(--light-green);
   border-radius: 10px;
   cursor: pointer;
-`;
-
-const StyledDialog = styled(Dialog)`
-
-`;
-
-const StyledInput = styled.input`
-  width: 100%;
-  border: none;
-  border-bottom: 1px solid black;
-  padding: 10px 3px;
-  transition: border-color 0.1s;
-
-  &:focus {
-    border-color: var(--red-highlight);
-  }
-`;
-
-const SubmitButton = styled.button`
-  display: flex;
-  align-items: center;
-  justify-content: space-around;
-  padding: 10px 15px;
-  margin-top: 15px;
-  width: 150px;
-`;
-
-const StyledDoorIcon = styled(DoorIcon)`
-  width: 50px;
-  height: 50px;
 `;
 
 export default Homepage;
---
+++ client/src/components/NewRoomDialog.js
@@ -0,0 +1,100 @@
+import React, { createRef } from 'react';
+import { useHistory } from 'react-router-dom'
+import styled from 'styled-components';
+
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Paper from '@material-ui/core/Paper';
+import Draggable from 'react-draggable';
+
+import { ReactComponent as DoorIcon } from '../assets/images/door.svg';
+
+function DraggableDialog(props) {
+  return (
+    <Draggable>
+        <Paper {...props} />
+    </Draggable>
+  )
+}
+
+function NewRoomDialog({ openDialog, setOpenDialog }) {
+
+  const history = useHistory();
+  const roomNameInput = createRef();
+
+  const handleClose = () => setOpenDialog(false);
+
+  const createNewRoom = event => {
+    event.preventDefault();
+
+    const roomName = roomNameInput.current.value;
+
+    history.push(`rooms/room/${roomName}`);
+  }
+
+  return (
+    <Dialog
+      open={openDialog}
+      onClose={handleClose}
+      PaperComponent={DraggableDialog}
+      aria-labelledby="draggable-dialog-title"
+    >
+      <DialogTitle style={{ cursor: 'move' }} id="draggable-dialog-title">
+        New Room
+      </DialogTitle>
+      <DialogContent>
+        <form onSubmit={createNewRoom}>
+          <DialogContentText name='room-name' htmlFor='room-name'>
+            Choose a Room Name to get Started!
+          </DialogContentText>
+          <StyledInput
+            id='room-name'
+            name='room-name'
+            type='text'
+            ref={roomNameInput}
+            required
+          />
+          <DialogActions>
+            <SubmitButton
+              type='submit'
+            >
+              <StyledDoorIcon />
+              Open
+            </SubmitButton>
+          </DialogActions>
+        </form>
+      </DialogContent>
+    </Dialog>
+  )
+}
+
+const StyledInput = styled.input`
+  width: 100%;
+  border: none;
+  border-bottom: 1px solid black;
+  padding: 10px 3px;
+  transition: border-color 0.1s;
+
+  &:focus {
+    border-color: var(--red-highlight);
+  }
+`;
+
+const SubmitButton = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  padding: 10px 15px;
+  margin-top: 15px;
+  width: 150px;
+`;
+
+const StyledDoorIcon = styled(DoorIcon)`
+  width: 50px;
+  height: 50px;
+`;
+
+export default NewRoomDialog;
