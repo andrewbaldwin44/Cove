c4385301102ca75b87502bbd9d169d109c293845
Andrew Baldwin <andrew.baldwin44@gmail.com>
2020-08-18 16:37:44 -0400

    Add style to Room component

--- client/src/components/Room/Main.js
+++ client/src/components/Room/Main.js
@@ -1,22 +1,72 @@
-import React, { useContext } from 'react';
+import React from 'react';
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
-import { AuthenticationContext } from '../AuthenticationContext';
+import { BsArrowLeftShort, BsSearch } from 'react-icons/bs';
+
+import DefaultBackground from '../../assets/images/default-background.jpeg';
 
 function Main({ isOwner }) {
-  const {
-    userData,
-  } = useContext(AuthenticationContext);
-
   return (
-    <div>
-      {isOwner ? (
-        `Welcome to your new room ${userData.displayName || userData.email}!`
-      ) : (
-        `Welcome to the room!`
-      )}
-    </div>
+    <Wrapper>
+      <Header>
+        <Link to='/'><BackArrow /></Link>
+        <SearchContainer>
+          <StyledSearchIcon />
+          <SearchInput type='text' placeholder='Search...' />
+        </SearchContainer>
+      </Header>
+    </Wrapper>
   )
 }
+
+const Wrapper = styled.div`
+  display: flex;
+  background-image: url(${DefaultBackground});
+  background-repeat: no-repeat;
+  background-size: cover;
+  height: 100vh;
+`;
+
+const Header = styled.div`
+  display: flex;
+  align-items: center;
+  height: 100px;
+  width: 100%;
+  padding: 0 10px;
+`;
+
+const BackArrow = styled(BsArrowLeftShort)`
+  font-size: 4.5em;
+  justify-self: flex-start;
+`;
+
+const SearchContainer = styled.div`
+  position: relative;
+  display: flex;
+  align-items: center;
+  margin: 0 auto;
+`;
+
+const SearchInput = styled.input`
+  width: 400px;
+  height: 40px;
+  color: inherit;
+  padding: 5px 10px;
+  padding-left: 40px;
+  outline: none;
+  border: none;
+  border-radius: 8px;
+  box-shadow: 1px 1px 5px var(--dark-shadow),
+              2px 2px 5px var(--dark-shadow);
+`;
+
+const StyledSearchIcon = styled(BsSearch)`
+  position: absolute;
+  height: 20px;
+  width: 20px;
+  margin-left: 10px;
+  color: inherit;
+`;
 
 export default Main;
