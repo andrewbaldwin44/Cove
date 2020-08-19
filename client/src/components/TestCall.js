33dc6a53ff9ccfd99dcac8ff07912117ea6e7b79
Andrew Baldwin <andrew.baldwin44@gmail.com>
2020-08-19 14:34:37 -0400

    Setup front-end socket.io

--- client/src/components/TestCall.js
+++ client/src/components/TestCall.js
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 import styled from 'styled-components';
 
-function TestCall() {
-  return (
-    <div>
+import io from 'socket.io-client';
 
-    </div>
+const socket = io.connect('http://localhost:4000');
+
+function TestCall() {
+  const { callID } = useParams();
+
+  useEffect(() => {
+    socket.emit('join-room', callID, 10);
+    // eslint-disable-next-line
+  }, [])
+
+  return (
+    <VideoGrid>
+
+    </VideoGrid>
   )
 }
+
+const VideoGrid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, 300px);
+  gird-auto-rows: 300px;
+`;
 
 export default TestCall;
--- server/handlers/socketHandlers.js
+++ server/handlers/socketHandlers.js
@@ -1,5 +1,7 @@
 function handleVideoCall(socket, io) {
-
+  socket.on('join-room', (roomID, userID) => {
+    console.log(roomID, userID);
+  });
 }
 
 module.exports = { handleVideoCall };
