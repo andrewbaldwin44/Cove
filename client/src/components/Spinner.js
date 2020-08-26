75be1ffb96bfe40dd8d5f4d0ae8767387d72160a
Andrew Baldwin <andrew.baldwin44@gmail.com>
2020-08-26 15:18:43 -0400

    Add spinner to loading times

--- client/src/components/Spinner.js
+++ client/src/components/Spinner.js
@@ -1,28 +1,26 @@
 import React from 'react';
-import styled, { keyframes } from "styled-components";
+import styled, { keyframes } from 'styled-components';
 
-import SpinnerImage from "../assets/images/spinner.png";
+import { AiOutlineLoading3Quarters } from 'react-icons/ai'
 
-function Spinner({ height }) {
-  height = height || '30px';
-
+function Spinner() {
   return (
-    <SpinnerIcon src={SpinnerImage} height={height} />
+    <SpinnerIcon />
   )
 }
 
 const Spin = keyframes`
   from {
-    transform: translate3d(-50%, -50%, 0) rotate(0deg);
+    transform: translate(-50%, -50%) rotate(0deg);
   }
   to {
-     transform: translate3d(-50%, -50%, 0) rotate(360deg);
+     transform: translate(-50%, -50%) rotate(360deg);
   }
 `;
 
-const SpinnerIcon = styled.img`
-  height: ${props => props.height};
-  width: auto;
+const SpinnerIcon = styled(AiOutlineLoading3Quarters)`
+  font-size: 1.4em;
+  color: var(--main-red);
   animation: ${Spin} 1s infinite linear;
 `;
 
