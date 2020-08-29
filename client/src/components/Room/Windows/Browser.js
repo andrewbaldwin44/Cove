8b0c88e823d8911479056cf63f8fb36ed9475621
Andrew Baldwin <andrew.baldwin44@gmail.com>
2020-08-28 20:08:23 -0400

    Add browser functionality

--- client/src/components/Room/Windows/Browser.js
+++ client/src/components/Room/Windows/Browser.js
@@ -1,9 +1,65 @@
-import React from 'react';
+import React, { createRef, useState } from 'react';
+import styled from 'styled-components';
+
+import AppHeader from './AppHeader';
+
+import { BsSearch } from 'react-icons/bs';
 
 function Browser() {
+  const searchInput = createRef();
+
+  const [url, setUrl] = useState('');
+
+  const getWebsite = event => {
+    event.preventDefault();
+
+    setUrl(searchInput.current.value);
+  }
+
   return (
-    <div>Browser</div>
+    <>
+      <AppHeader>
+        <form onSubmit={getWebsite}>
+          <Search
+            type='text'
+            ref={searchInput}
+            placeholder='Url'
+          />
+          <SearchButton
+            type='submit'
+          >
+              <BsSearch />
+          </SearchButton>
+        </form>
+      </AppHeader>
+      <StyledIframe
+        src={url}
+        width='100%'
+      />
+    </>
   )
 }
+
+const Search = styled.input`
+  height: 40px;
+  width: 70vw;
+  padding-right: 2px;
+  font-size: 1em;
+`;
+
+const SearchButton = styled.button`
+  width: 70px;
+  height: 40px;
+  background-color:#4B515D;
+
+  svg {
+    color: white;
+  }
+`;
+
+const StyledIframe = styled.iframe`
+  margin-top: 70px;
+  height: calc(100% - 50px);
+`;
 
 export default Browser;
