f835c209480c9855935373bb2835e258b23cae9d
Andrew Baldwin <andrew.baldwin44@gmail.com>
2020-08-29 23:25:08 -0400

    Add correct formatting to minutes

--- client/src/components/Room/Windows/Activities/CircularProgress.js
+++ client/src/components/Room/Windows/Activities/CircularProgress.js
@@ -6,12 +6,15 @@ import useInterval from '../../hooks/useInterval';
 import TimeSelect from './TimeSelect';
 
 function formatTimeLeft(time) {
-  const minutes = Math.floor(time / 60);
+  let minutes = Math.floor(time / 60);
 
   let seconds = time % 60;
 
   if (seconds < 10) {
     seconds = `0${seconds}`;
+  }
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
   }
 
   return `${minutes}:${seconds}`;
